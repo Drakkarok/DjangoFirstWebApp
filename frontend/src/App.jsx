5fa812148f0e21103244402da65a708f4399a5bf
Drakkarok <52042893+Drakkarok@users.noreply.github.com>
2024-07-11 15:35:45 +0300

    V1.1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import react from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
@@ -6,10 +6,9 @@ import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import ProtectedRoute from "./components/ProtectedRoute"
 
-
 function Logout() {
   localStorage.clear()
-  return <Navigate to = "/login" />
+  return <Navigate to="/login" />
 }
 
 function RegisterAndLogout() {
@@ -30,7 +29,6 @@ function App() {
           }
         />
         <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/register" element={<RegisterAndLogout />} />
         <Route path="*" element={<NotFound />}></Route>
